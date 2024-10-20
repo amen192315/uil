2aba34e5876bb7f7fc9f6c3ff8a3ca79507fd635
Vladislav <amen192315@gmail.com>
2024-10-20 17:25:03 +0300

    just commit

---
+++ main.js
@@ -0,0 +1,8 @@
+if (window.innerWidth <= 1200) {
+  let box = document.querySelector('.box');
+  box.addEventListener('click', boxHandler);
+  function boxHandler(e) {
+    e.preventDefault();
+    let
+  }
+}
